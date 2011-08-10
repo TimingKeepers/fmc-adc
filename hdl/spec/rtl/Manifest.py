7577ea5e14785ff7d514c1ebc2bc8f2cdecab46e
mcattin <mcattin@ddd67a1a-c8ad-4635-afe9-0b8a11d8f8e4>
2011-08-10 06:13:10 +0000

    Reorganise dirs.
    git-svn-id: http://svn.ohwr.org/fmc-adc-100m14b4cha/trunk@62 ddd67a1a-c8ad-4635-afe9-0b8a11d8f8e4

--- hdl/spec/rtl/Manifest.py
+++ hdl/spec/rtl/Manifest.py
@@ -5,8 +5,8 @@ files = [
 
 modules = {
     "local" : "../../adc/rtl",
-    "svn" : [ "http://svn.ohwr.org/DDRCONTROLLER",
-              "http://svn.ohwr.org/GENNUMCORE"],
+    "svn" : [ "http://svn.ohwr.org/ddr3-sp6-core/trunk/hdl/rtl",
+              "http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl"],
     "git" : "git://ohwr.org/hdl-core-lib/general-cores.git"}
 
 fetchto="../ip_cores"
