258a3330e774890e1e72a2d77ffe39e6112aeacb
mcattin <mcattin@ddd67a1a-c8ad-4635-afe9-0b8a11d8f8e4>
2011-08-10 07:49:29 +0000

    Reorganise files in dir.
    git-svn-id: http://svn.ohwr.org/fmc-adc-100m14b4cha/trunk@66 ddd67a1a-c8ad-4635-afe9-0b8a11d8f8e4

--- hdl/spec/rtl/Manifest.py
+++ hdl/spec/rtl/Manifest.py
@@ -1,12 +1,12 @@
 files = [
     "spec_top_fmc_adc_100Ms.vhd",
-    "fmc_adc_100Ms_csr.vhd",
-    "../spec_top_fmc_adc_100Ms.ucf"];
+    "fmc_adc_100Ms_csr.vhd"];
 
 modules = {
     "local" : "../../adc/rtl",
     "svn" : [ "http://svn.ohwr.org/ddr3-sp6-core/trunk/hdl/rtl",
-              "http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl"],
+              "http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl",
+              "http://svn.ohwr.org/gn4124-core/trunk/hdl/common/rtl"],
     "git" : "git://ohwr.org/hdl-core-lib/general-cores.git"}
 
 fetchto="../ip_cores"
