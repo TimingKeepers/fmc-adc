5e475118a964a98b9735a37e073e26331482b8e8
Matthieu Cattin <matthieu.cattin@cern.ch>
2013-03-12 09:10:41 +0100

    syn: Firmware release 1.0, synthesis reports and sdb meta-info.

--- hdl/spec/rtl/sdb_meta_pkg.vhd
+++ hdl/spec/rtl/sdb_meta_pkg.vhd
@@ -21,14 +21,14 @@ package sdb_meta_pkg is
     -- Top module name (string, 16 char)
     syn_module_name  => "spec_top_fmc_adc",
     -- Commit ID (hex string, 128-bit = 32 char)
-    -- git log -1 --format="%H" | cut -c1-31
-    syn_commit_id    => "150b83db8fa9e0ff9050166b7695ee9a",
+    -- git log -1 --format="%H" | cut -c1-32
+    syn_commit_id    => "baa41197a02acc5cbdfbc5c893849b40",
     -- Synthesis tool name (string, 8 char)
     syn_tool_name    => "ISE     ",
     -- Synthesis tool version (bcd encoded, 32-bit)
     syn_tool_version => x"00000133",
     -- Synthesis date (bcd encoded, 32-bit)
-    syn_date         => x"20130307",
+    syn_date         => x"20130312",
     -- Synthesised by (string, 15 char)
     syn_username     => "mcattin        ");
 
@@ -38,7 +38,7 @@ package sdb_meta_pkg is
       vendor_id => x"000000000000CE42",  -- CERN
       device_id => x"47c786a2",          -- echo "spec_fmc-adc-100m14b4cha" | md5sum | cut -c1-8
       version   => x"00010000",          -- bcd encoded, [31:16] = major, [15:0] = minor
-      date      => x"20130307",          -- yyyymmdd
+      date      => x"20130312",          -- yyyymmdd
       name      => "spec_fmcadc100m14b "));
 
 
