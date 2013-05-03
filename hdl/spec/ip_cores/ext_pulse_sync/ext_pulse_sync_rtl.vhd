2d06837c2beb2ab0bed1569bb3b38fbeb7bdd9b6
Matthieu Cattin <matthieu.cattin@cern.ch>
2013-05-03 16:27:33 +0200

    hdl: Add missing GPL header.

--- hdl/spec/ip_cores/ext_pulse_sync/ext_pulse_sync_rtl.vhd
+++ hdl/spec/ip_cores/ext_pulse_sync/ext_pulse_sync_rtl.vhd
@@ -41,7 +41,19 @@ use work.utils_pkg.all;
 --! 27.10.2009    mcattin     Possibility for output monostable to be
 --!                           retriggerable
 --! 03.03.2011    mcattin     Input polarity from port instead of generic
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- GNU LESSER GENERAL PUBLIC LICENSE
+--------------------------------------------------------------------------------
+-- This source file is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as published by the
+-- Free Software Foundation; either version 2.1 of the License, or (at your
+-- option) any later version. This source is distributed in the hope that it
+-- will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+-- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-- See the GNU Lesser General Public License for more details. You should have
+-- received a copy of the GNU Lesser General Public License along with this
+-- source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
+--------------------------------------------------------------------------------
 --! @todo
 --
 -------------------------------------------------------------------------------
