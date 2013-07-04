00c50f919fa159a4573f43b4cc03e3a556caccc0
Matthieu Cattin <matthieu.cattin@cern.ch>
2013-07-04 17:20:40 +0200

    hdl: Move ip_cores dir outside of the spec dir.
    The ip_cores dir is meant to be shared between different carriers.

--- hdl/spec/rtl/Manifest.py
+++ hdl/spec/rtl/Manifest.py
@@ -5,12 +5,4 @@ files = [
     "timetag_core.vhd",
     "irq_controller_regs.vhd",
     "irq_controller.vhd",
-    "sdb_meta_pkg.vhd"];
-
-modules = {
-    "local" : "../../adc/rtl",
-    "git" : ["git://ohwr.org/hdl-core-lib/general-cores.git::sdb_extension",
-             "git://ohwr.org/hdl-core-lib/ddr3-sp6-core.git::spec_bank3_64b_32b",
-             "git://ohwr.org/hdl-core-lib/gn4124-core.git::master"]}
-
-fetchto="../ip_cores"
+    "sdb_meta_pkg.vhd"]
--- hdl/spec/syn/Manifest.py
+++ hdl/spec/syn/Manifest.py
@@ -7,13 +7,20 @@ syn_package = "fgg484"
 syn_top = "spec_top_fmc_adc_100Ms"
 syn_project = "spec_fmc_adc_100Ms.xise"
 
-files = ["../spec_top_fmc_adc_100Ms.ucf",
-         "../ip_cores/adc_sync_fifo.ngc",
-         "../ip_cores/multishot_dpram.ngc",
-         "../ip_cores/wb_ddr_fifo.ngc",
-         "../ip_cores/adc_serdes.vhd",
-         "../ip_cores/monostable/monostable_rtl.vhd",
-         "../ip_cores/ext_pulse_sync/ext_pulse_sync_rtl.vhd",
-         "../ip_cores/utils/utils_pkg.vhd"]
+files = [
+    "../spec_top_fmc_adc_100Ms.ucf",
+    "../../ip_cores/adc_sync_fifo.ngc",
+    "../../ip_cores/multishot_dpram.ngc",
+    "../../ip_cores/wb_ddr_fifo.ngc",
+    "../../ip_cores/adc_serdes.vhd",
+    "../../ip_cores/monostable/monostable_rtl.vhd",
+    "../../ip_cores/ext_pulse_sync/ext_pulse_sync_rtl.vhd",
+    "../../ip_cores/utils/utils_pkg.vhd",]
 
-modules = { "local" : "../rtl" }
+modules = { "local" : ["../rtl",
+                       "../../adc/rtl"],
+            "git" : ["git://ohwr.org/hdl-core-lib/general-cores.git::sdb_extension",
+                     "git://ohwr.org/hdl-core-lib/ddr3-sp6-core.git::spec_bank3_64b_32b",
+                     "git://ohwr.org/hdl-core-lib/gn4124-core.git::master"]}
+
+fetchto="../../ip_cores"
